addce1b90983e39c802fa2ae5bc9cbf0774d5657
Leon de Vries <l.r.devries@utwente.nl>
2021-02-15 17:16:15 +0000

    Revert "Initial commit"
    This reverts commit cf07a82de71f525157770c920eba6751af6c4d54

--- src/main/java/nl/utwente/soa/sampleWebapplication/web/IndexController.java
+++ src/main/java/nl/utwente/soa/sampleWebapplication/web/IndexController.java
@@ -1,37 +1,21 @@
 package nl.utwente.soa.sampleWebapplication.web;
 
-import nl.utwente.soa.sampleWebapplication.services.EchoProxyService;
+import nl.utwente.soa.sampleWebapplication.services.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.websocket.EncodeException;
-import java.io.IOException;
 
 @Controller
 public class IndexController {
 
-    @Qualifier("rest")
-    @Autowired private EchoProxyService echoProxyService;
+    @Autowired private WeatherService weatherService;
 
     @GetMapping("/")
     public String showWeather(Model model) {
-        return "echo-form";
-    }
 
-    @RequestMapping(value = "/proxy", method= RequestMethod.POST)
-    public String processForm(@ModelAttribute(value="message") String message) throws IOException, EncodeException {
-        echoProxyService.echo(message);
-        return "result";
-    }
+        model.addAttribute("weather", weatherService.getCurrentWeatherByHand("Amsterdam,NL"));
 
-    @GetMapping("/messageboard")
-    public String showMessageBoard(Model model) {
-        return "messageboard";
+        return "show-weather";
     }
 }
